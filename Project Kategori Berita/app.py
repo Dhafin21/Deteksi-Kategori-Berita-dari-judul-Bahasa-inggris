6351558e4408508548c54b6602f873d8513bafe5
Dhafin21 <86392758+Dhafin21@users.noreply.github.com>
2025-01-06 20:58:53 +0700

    app.py

--- Project Kategori Berita/app.py
+++ Project Kategori Berita/app.py
@@ -1,32 +1,41 @@
 import streamlit as st
-import joblib
 import re
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Memuat model dan TfidfVectorizer
-model = joblib.load('/content/model_kategori_berita_pickle.pkl')
-vectorizer = joblib.load('/content/vectorizer_kategori_berita_pickle.pkl')
+# Fungsi untuk memuat model dan vektor
+@st.cache_resource  # Caching untuk mempercepat load saat aplikasi dijalankan ulang
+def load_model_and_vectorizer():
+    with open("model/news_classifier.pkl", "rb") as model_file:
+        model = pickle.load(model_file)
+    with open("model/tfidf_vectorizer.pkl", "rb") as vectorizer_file:
+        vectorizer = pickle.load(vectorizer_file)
+    return model, vectorizer
+
+# Memuat model dan vectorizer
+model, vectorizer = load_model_and_vectorizer()
 
 # Fungsi untuk membersihkan teks
 def clean_text(text):
     text = text.lower()
-    text = re.sub(r'[^a-z\s]', '', text)
+    text = re.sub(r'[^a-z\s]', '', text)  # Hanya menyisakan huruf kecil dan spasi
     return text
 
 # Fungsi untuk prediksi
 def predict_category(input_text):
     cleaned_input = clean_text(input_text)
-    input_tfidf = vectorizer.transform([cleaned_input])
+    input_tfidf = vectorizer.transform([cleaned_input])  # Mengubah teks menjadi vektor TF-IDF
     predicted_category = model.predict(input_tfidf)
     return predicted_category[0]
 
 # Streamlit UI
 st.title('Klasifikasi Kategori dari Judul Berita')
-input_text = st.text_area("Masukan Judul Berita")
+input_text = st.text_area("Masukkan Judul Berita")
 
-# Button to trigger sentiment analysis
+# Tombol untuk melakukan analisis
 if st.button('Analisis Judul Berita'):
     if input_text:
         predicted_category = predict_category(input_text)
-        st.write(f"Kategori Berita adalah: {predicted_category}")
+        st.write(f"Kategori Berita adalah: **{predicted_category}**")
     else:
         st.write("Silakan masukkan Judul Berita.")
